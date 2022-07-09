3623112ad10b931454756f70bd0c197443d4ec14
yemrebarmanbek <82896678+yemrebarmanbek@users.noreply.github.com>
2022-07-09 21:03:49 +0300

    aaa
    sss

---
+++ Otogaleri Otomasyonu/Otogaleri Otomasyonu/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace Otogaleri_Otomasyonu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(32, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ruhsat No";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(32, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Marka";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(32, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Model";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(32, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Yakıt Tipi";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(154, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(137, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Toyota",
+            "Honda",
+            "Opel"});
+            this.comboBox1.Location = new System.Drawing.Point(154, 46);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(137, 24);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(154, 83);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(137, 24);
+            this.comboBox2.TabIndex = 6;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Benzinli",
+            "Dizel",
+            "Benzimli ve Tüplü"});
+            this.comboBox3.Location = new System.Drawing.Point(154, 122);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(137, 24);
+            this.comboBox3.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(323, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 30);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "ARA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(423, 46);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 24);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Kasa Tipi";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(423, 83);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(102, 24);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Kilometresi";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(423, 122);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 24);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Fiyatı";
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "Sedan",
+            "Hatchback"});
+            this.comboBox4.Location = new System.Drawing.Point(560, 46);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(153, 24);
+            this.comboBox4.TabIndex = 12;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(560, 85);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(153, 22);
+            this.textBox2.TabIndex = 13;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(560, 124);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(153, 22);
+            this.textBox3.TabIndex = 14;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(99, 200);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 28);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Kaydet";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(302, 200);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 28);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Sil";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(489, 200);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(178, 28);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Fiyat Güncelle";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(21, 264);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1014, 158);
+            this.dataGridView1.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1060, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "OTO GALERİ PROGRAMI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Otogaleri Otomasyonu/Otogaleri Otomasyonu/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Otogaleri_Otomasyonu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        OleDbConnection baglantim = new OleDbConnection("Provider = Microsoft.ACE.OleDb.12.0;Data Source=" + Application.StartupPath + "\\otogaleri.accdb;Persist Security Info=False");
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Green;
+            kayitlari_listele();
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) // marka comboboxı
+        {
+            comboBox2.Items.Clear(); // her yeni marka ifadesine göre model combocoxını temizliyoruz
+            string marka = comboBox1.SelectedItem.ToString();
+
+            if (marka == "Toyota")
+            {
+                string[] model = { "Auris", "Yaris", "Corolla" };
+                comboBox2.Items.AddRange(model);
+            }
+            if (marka == "Honda")
+            {
+                string[] model = { "Cıvıc", "Accord" };
+                comboBox2.Items.AddRange(model);
+            }
+            if (marka == "Opel")
+            {
+                string[] model = { "Astra", "Vectra", "Corsa" };
+                comboBox2.Items.AddRange(model);
+            }
+        }
+
+        private void kayitlari_listele()  // her durumda acces verisini data gride yansıtmak için metot kullanırız
+        {
+            try
+            {
+                baglantim.Open();
+                OleDbDataAdapter liste = new OleDbDataAdapter("select *from araclar", baglantim);
+                DataSet hafiza = new DataSet();
+                liste.Fill(hafiza);  // access verilerini formdaki veri tablosuna attık fill doldur anlamına geliyor
+                dataGridView1.DataSource = hafiza.Tables[0];
+                baglantim.Close();
+            }
+            catch (Exception hatamesaj)
+            {
+                MessageBox.Show(hatamesaj.Message);
+                baglantim.Close();
+
+
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) //kaydet butonu
+        {
+            // bu buton ile form da girilen bilgileri acces veritabanına kaydedeceğiz
+            try
+            {
+                baglantim.Open();
+                OleDbDataAdapter ekle_komutu = new OleDbDataAdapter("insert into araclar (ruhsatno,marka,model,yakit_tipi,kasa_tipi,kilometre,fiyat) values('" + textBox1.Text + "','" + comboBox1.SelectedItem.ToString() + "','" + comboBox2.SelectedItem.ToString() + "','" + comboBox3.SelectedItem.ToString() + "','" + comboBox4.SelectedItem.ToString() + "','" + textBox2.Text + "','" + textBox3.Text + "')", baglantim);
+                DataSet hafiza = new DataSet();
+                ekle_komutu.Fill(hafiza);
+                baglantim.Close();
+                MessageBox.Show("araç veri tabanına eklendi");
+                textBox1.Clear(); textBox2.Clear(); textBox3.Clear();
+                kayitlari_listele();
+            }
+            catch (Exception hatamesaj)
+            {
+                MessageBox.Show(hatamesaj.Message);
+                baglantim.Close();
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e) // sil komutu
+        {
+            try
+            {
+                baglantim.Open();
+                OleDbDataAdapter sil_komutu = new OleDbDataAdapter("delete from araclar where ruhsatno='" + textBox1.Text + "'", baglantim);
+                DataSet hafiza = new DataSet();
+                sil_komutu.Fill(hafiza);
+                baglantim.Close();
+                MessageBox.Show("araç veri tabanından silindi");
+                kayitlari_listele();
+
+            }
+            catch (Exception hatamesaj)
+            {
+                MessageBox.Show(hatamesaj.Message);
+                baglantim.Close();
+                
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e) // fiyat guncelle butonu
+        {
+            try
+            {
+                baglantim.Open();
+                OleDbDataAdapter guncelle = new OleDbDataAdapter("update araclar set fiyat='" + textBox3.Text + "' where ruhsatno='" + textBox1.Text + "'", baglantim);
+                DataSet hafiza = new DataSet();
+                guncelle.Fill(hafiza);
+                baglantim.Close();
+                MessageBox.Show("araç fiyatları güncellendi");
+                kayitlari_listele();
+
+
+            }
+            catch (Exception mesaj)
+            {
+                MessageBox.Show(mesaj.Message);
+                baglantim.Close();
+                
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) // ara butonu
+        {
+            try
+            {
+                baglantim.Open();
+                OleDbDataAdapter ara = new OleDbDataAdapter("select *from araclar where ruhsatno='" + textBox1.Text + "'", baglantim);
+                DataSet hafiza = new DataSet();
+                ara.Fill(hafiza);
+                dataGridView1.DataSource = hafiza.Tables[0];
+                baglantim.Close();
+
+            }
+            catch (Exception mesaj)
+            {
+
+                MessageBox.Show(mesaj.Message);
+                baglantim.Close();
+
+            }
+        }
+    }
+}
+
+          
